9d6ba1969f81b3fd78ff3ba5ca2147fd534829bb
John Obi <johnny9334@ymail.com>
2017-02-10 08:19:18 -0800

    Added the fizzbuzz function

---
+++ app/library.js
@@ -0,0 +1,18 @@
+
+
+function fizzBuzz(num)
+{
+	if((num%3 === 0) && (num%5 === 0))
+	{
+		return 'FizzBuzz';
+	}
+
+	if(num%3 === 0)
+	{
+		return 'Fizz';
+	}else if(num%5 === 0){
+		return 'Buzz';
+	}else{
+		return num;
+	}
+}
